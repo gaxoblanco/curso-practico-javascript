74fcc26b1ba06978855d775e6cf80d012056583f
gaxoblanco <94192138+gaxoblanco@users.noreply.github.com>
2022-02-04 21:24:08 -0300

    Ahora tiene una mejor precentacion y tiene cupones, falta definir bien el primer if de la sentencia, ya que no supe como diferencias numeros de string

--- descuentos.js
+++ descuentos.js
@@ -13,9 +13,52 @@ function ButtonPricesDiscount(){
     const priceValue = inputPrice.value;
     const inputDiscount = document.getElementById("InputDiscount");
     const discountValue = inputDiscount.value;
+//Opcion para cupones
+    const cupones= ["Platzi10", "Platzi15", "Platzi25", "Platzi30", "Platzi50"];
+    
+    if (InputDiscount = 15 ){
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio final es $" + precioConDescuento;
+    }else {
+        if(!copunes.includes(InputDescuento)){
+        resultP.innerText = "Cupon Invalido" + precioConDescuento;
+            }else{switch (InputDescuento){
+                case "Platzi10":
+                    const precioConDescuento = calcularPrecioConDescuento (priceValue, 10);
+                    const resultP = document.getElementById("ResultP");
+                    resultP.innerText = "El precio final es $" + precioConDescuento;
+                break;
 
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio final es $" + precioConDescuento;
+                case "Platzi15":
+                    const precioConDescuento = calcularPrecioConDescuento(priceValue, 15);
+                    const resultP = document.getElementById("ResultP");
+                    resultP.innerText = "El precio final es $" + precioConDescuento;
+                break;
+
+                case "Platzi25":
+                    const precioConDescuento = calcularPrecioConDescuento(priceValue, 25);
+                    const resultP = document.getElementById("ResultP");
+                    resultP.innerText = "El precio final es $" + precioConDescuento;
+                break;
+
+                case "Platzi30":
+                    const precioConDescuento = calcularPrecioConDescuento(priceValue, 30);
+                    const resultP = document.getElementById("ResultP");
+                    resultP.innerText = "El precio final es $" + precioConDescuento;
+                break;
+
+                case "Platzi50":
+                    const precioConDescuento = calcularPrecioConDescuento(priceValue, 50);
+                    const resultP = document.getElementById("ResultP");
+                    resultP.innerText = "El precio final es $" + precioConDescuento;
+                break;
+                }
+            }
+
+    }
+
 }
+
+
