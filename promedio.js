db04c040919934c8b03b6f820f71a5ddecdb9d98
gaxoblanco <94192138+gaxoblanco@users.noreply.github.com>
2022-02-05 14:52:08 -0300

    Commit con explicaciones, ahora cargo el codigo sobre los comentarios

--- promedio.js
+++ promedio.js
@@ -1,3 +1,5 @@
+//promedio aritmetica
+
 const lista1 = [
     100,
     200,
@@ -21,6 +23,8 @@ function calcularMediaAritmetica(lista){
      //   sumaLista = sumaLista + lista[i];
     //};
 
+
+    //---------Metodo reducido para calcular el array----------//
 //reduce recibe una funciona y este metodo nos permite ir sumando cada uno de nuestros elementos
     const sumaLista = lista.reduce(
         // valor acumulado es la suma de las iteraciones
@@ -31,4 +35,37 @@ function (valorAcumulado = 0, nuevoElemento){
 
 const promedioLista = sumaLista / lista.length;
 return promedioLista;
+}
+
+/// termina promedio aritmetica 
+
+
+
+// Promedio Mediana
+const lista2 = [
+    250,
+    160,
+    400,
+    5000000,
+];
+
+const mitadLista2 = lista2.length / 2;
+
+function esPar(numerito){
+//esto dice que si al divdir NO tiene decimales es un numero par. => 6/2 = 3 (tres es un numero entero,) 5/2 = 2.5 (2.5 es con decimales)
+    if(numerito % 2 === 0){
+        return true;
+    } else{
+        return false;
+    }
+}
+//primero que nada tenemos que saber si la lista es par o inpar, si es para el centro de la lista son 2 elementos que se suman y dividen a la mitadLista2
+
+if (lista2 es par?){
+    //necesutamos dos elementos
+    // -> el promedio
+    // -> mediana
+
+}else{
+    //posicion mitadLista2 dentro lista2.
 }
